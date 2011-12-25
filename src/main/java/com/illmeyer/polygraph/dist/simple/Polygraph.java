ab9ea3f4e09cadb9bdb5ec5b07fc0220a1e03bb2
Wolfgang Illmeyer <wolfgang@illmeyer.com>
2011-12-26 00:20:05 +0100

    Assembly configuration

---
+++ src/main/java/com/illmeyer/polygraph/dist/simple/Polygraph.java
@@ -0,0 +1,12 @@
+package com.illmeyer.polygraph.dist.simple;
+
+public class Polygraph {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
