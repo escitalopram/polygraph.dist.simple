83979347a73dd501349d355b615be9b2ebda71b0
Wolfgang Illmeyer <wolfgang@illmeyer.com>
2012-01-01 21:27:25 +0100

    Bugfixes from first Integration test

--- src/main/java/com/illmeyer/polygraph/dist/simple/Polygraph.java
+++ src/main/java/com/illmeyer/polygraph/dist/simple/Polygraph.java
@@ -1,12 +1,32 @@
 package com.illmeyer.polygraph.dist.simple;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+
+import com.illmeyer.polygraph.core.DefaultGunConfigurator;
+import com.illmeyer.polygraph.core.Gun;
+
 public class Polygraph {
 
 	/**
 	 * @param args
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+	public static void main(String[] args) throws IOException {
+		Resource res = new FileSystemResource(args[1]);
+		XmlBeanFactory xbf = new XmlBeanFactory(res);
 
+		DefaultGunConfigurator gc = new DefaultGunConfigurator();
+		gc.setActiveTemplate(args[0]);
+		gc.initialize();
+		Gun g = (Gun)xbf.getBean("gun");
+		g.setConfigurator(gc);
+		g.configure(gc);
+		g.initialize();
+		g.trigger();
+		g.destroy();
+	}
 }
